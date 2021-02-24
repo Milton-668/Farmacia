bffe868073dc6fb0a0c6b86be6f6270c994d65c9
Milton-668 <miltonmjcp@gmail.com>
2021-02-23 21:10:34 -0300

    Farmacia

---
+++ farmacia-api/farmacia/src/main/java/com/farmacia/farmacia/Model/Produto.java
@@ -0,0 +1,77 @@
+package com.farmacia.farmacia.Model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "produto")
+public class Produto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	@NotNull
+	@Size(min = 2, max = 100)
+	private String nome;
+
+	@NotNull
+	private long preco;
+
+	@NotNull
+	@Size(min = 2, max = 100)
+	private String consumo;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public long getPreco() {
+		return preco;
+	}
+
+	public void setPreco(long preco) {
+		this.preco = preco;
+	}
+
+	public String getConsumo() {
+		return consumo;
+	}
+
+	public void setConsumo(String consumo) {
+		this.consumo = consumo;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+}
